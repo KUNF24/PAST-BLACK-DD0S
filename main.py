61efa9c29ceec5bcf76ef6e0d54419af1bd9c110
KUNF24 <kunf5616@gmail.com>
2024-12-18 08:53:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -52,11 +52,6 @@ def display_header():
     for line in header_lines:
         print(line)
       
-    # Versi dan URL
-    print(f"{Fore.WHITE}{Style.BRIGHT}{' ' * 57}v.1.0")
-    print(f"{Fore.CYAN}{Style.BRIGHT}{' ' * 16}https://github.com/KUNF24/PASTBLACK-DD0S.git")
-    print(f"{Fore.CYAN}|{'=' * 74}|")
-
 # Fungsi untuk Meminta Input dari Pengguna dengan Tampilan Rapi
 def get_user_input(prompt_message):
     print(f"{Fore.GREEN}|{' ' * 4}[?] {prompt_message.ljust(63)}|")
